4577a66287aeb3f4295b794d801095b941c507c9
LukeHoward4 <lwhby7@umsystem.edu>
2024-04-19 13:27:15 -0500

    Added documentation and made Prims in main class instead of Graph.java.

--- src/Graph.java
+++ src/Graph.java
@@ -29,41 +29,4 @@ public class Graph {
     public List<Edge> getAdjacencies(int vertex){
         return this.adjacencyList.get(vertex);
     }
-    public void Prims(Graph g){
-        int size = V;
-        int[] isKnown = new int[size+1];
-        float[] D = new float[size+1];
-        for(int i = 1; i < size+1; i++) {
-            D[i] = 9999999;
-        }
-        D[1] = 0;
-        isKnown[1] = 1;
-        Heap myHeap = new Heap(D,size);
-        while(myHeap.min_key() != -1){
-            int currentV = myHeap.min_id();
-            isKnown[currentV] = 1;
-            float dist = myHeap.key(myHeap.findId(currentV));
-            myHeap.delete_min();
-            List<Edge> currentEdges = this.adjacencyList.get(currentV);
-            Edge theEdge = null;
-            for(int i = 0; i < currentEdges.size(); i++){
-                Edge itrEdge = currentEdges.get(i);
-                if(itrEdge.getLength() == dist){
-                    theEdge = itrEdge;
-                    theEdge.printInfo();
-                }
-                else{
-                    int otherV = itrEdge.findOtherV(currentV);
-                    if(isKnown[otherV] == 0){
-                        //If this is a new edge
-                        if(itrEdge.getLength() < D[otherV]){
-                            //If this edge provides a shorter path to the vertex, decrease that vertex's key in the heap.
-                            D[otherV] = itrEdge.getLength();
-                            myHeap.decrease_key(myHeap.findId(otherV), itrEdge.getLength());
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
